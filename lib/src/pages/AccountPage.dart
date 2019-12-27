28d8450ed4e9b91f27fa317792565e1020723ecb
devfer <ferkki@hotmail.com>
2019-12-27 17:36:01 -0400

    estructura generada

---
+++ lib/src/pages/AccountPage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AccountPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text('Hola mundo!'),
+      ),
+    );
+  }
+}
